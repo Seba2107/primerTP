f90786582f37ef1264700726d3646e8bd72ac654
seba <sebastianmombelli81@gmail.com>
2023-11-11 02:34:19 -0300

    ultimas correcciones tp 11-11-23

--- scripts2.js
+++ scripts2.js
@@ -2,7 +2,6 @@
 const ticket = 200;
 
 // descuentos
-
 let descMenores = 0.80;
 let descJubilados = 0.50;
 let descEstudiante = 0.15;
@@ -12,10 +11,10 @@ const cantidad = document.getElementById('cantidad');
 const categoria = document.getElementById('categoria');
 const totalPagar = document.getElementById('totalAPagar');
 const btnResumen = document.getElementById('resumen');
+const btnComprar = document.getElementById('comprar');
 
 // funcion para calcular
 function calcularPago() {
-
     let total = cantidad.value * ticket;
     switch (categoria.value) {
         case "Menores":
@@ -28,13 +27,42 @@ function calcularPago() {
             total = total - (total * descEstudiante);
             break;
     }
-
-    totalPagar.textContent = `Total a Pagar: $ ${total}`
+    totalPagar.textContent = `Total a Pagar: $ ${total}`;
 }
-
-let flecha = () => { }
 
 btnResumen.addEventListener('click', (evento) => {
     evento.preventDefault();
     calcularPago();
 });
+
+btnComprar.addEventListener('click', (evento) => {
+    evento.preventDefault();
+    if (validarFormulario()) {
+        procesarCompra();
+    } else {
+        alert('Por favor, completa todos los campos.');
+    }
+});
+
+function validarFormulario() {
+    const nombre = document.getElementById('nombre').value;
+    const apellido = document.getElementById('apellido').value;
+    const correo = document.getElementById('correo').value;
+    const cantidad = document.getElementById('cantidad').value;
+    const categoria = document.getElementById('categoria').value;
+
+    if (nombre === '' || apellido === '' || correo === '' || cantidad === '' || categoria === '') {
+        return false;
+    }
+
+    // Puedes agregar más lógica de validación según tus necesidades
+
+    return true;
+}
+
+function procesarCompra() {
+    // Aquí puedes colocar la lógica para procesar la compra
+    // Puedes llamar a la función calcularPago() u otras operaciones necesarias
+    calcularPago();
+    alert('Compra realizada con éxito. ¡Gracias!');
+}
